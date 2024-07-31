3162a4c1e8773285f609bcfb6fbdeaf6edf2528c
Vincent <vravanel@gmail.com>
2024-07-31 12:56:14 +0200

    modification readme + index

--- Readme.md
+++ Readme.md
@@ -1,5 +1,21 @@
-Pour éxécuter la fonction dans le terminal :
+## Contexte
 
-```php
+Ce script PHP est conçu pour traiter le flux de produits de AdoreMe, disponible ici : [https://gw-services.prd.adoreme.com/v2/feeds/veesual](https://gw-services.prd.adoreme.com/v2/feeds/veesual). Il télécharge le flux JSON, le traite, et génère un fichier JSON contenant les options disponibles pour chaque produit.
+
+## Objectif
+
+L'objectif est de créer un format standardisé pour chaque produit avec les options disponibles. Il faut sauvegarder le fichier JSON de sortie sur le disque dur.
+
+## Exécuter le script
+
+Pour exécuter le script depuis le terminal, utilisez la commande suivante :
+
+```bash
 php index.php
 ```
+
+### Difficultés rencontrées
+
+Dans le cadre de ce test technique, j'ai rencontré particulièrement une difficulté sur le regroupement des produits "regular" et "plus".
+
+J'ai utilisé le nom du produit pour retirer " Plus" et regrouper les versions "regular" et "plus", mais je pense que cette méthode n'est pas fiable à 100%. Je n'ai malheureusement pas pu trouvé mieux.
--- index.php
+++ index.php
@@ -6,30 +6,118 @@ function productData()
     $products = json_decode($data, true);
 
     $productsData = [];
+    $productNames = [];
 
     foreach ($products as $product) {
         $productId = $product['id'];
+        $productName = str_replace(' Plus', '', $product['title']);
+        if (!isset($productNames[$productName])) {
+            $productNames[$productName] = [];
+        }
+        $productNames[$productId][] = $product;
+    }
+
+    $sizeOrder = ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL', '1X', '2X', '3X', '4X', '38', '40', '42', '44', '46'];
+    $cupOrder = ['A', 'B', 'C', 'D', 'DD', 'DDD', 'G', 'H', 'I'];
+
+    $sizeWeights = array_flip($sizeOrder);
+    $cupWeights = array_flip($cupOrder);
+
+    foreach ($productNames as $productName => $productVersions) {
         $transformedProduct = [];
 
-        if (isset($product['tops'])) {
-            $bandSizes = [];
-            $cupSizes = [];
-            $possibleTopValues = [];
+        $bandSizes = [];
+        $cupSizes = [];
+        $topSizes = [];
+        $bottomSizes = [];
+        $possibleTopValues = [];
+        $possibleBottomValues = [];
 
-            foreach ($product['tops'] as $top) {
-                if (!empty($top['band']) && !empty($top['cup'])) {
-                    $bandSizes[] = $top['band'];
-                    $cupSizes[] = $top['cup'];
-                    $possibleTopValues[] = [
-                        'band size' => $top['band'],
-                        'cup size' => $top['cup']
-                    ];
+        foreach ($productVersions as $product) {
+            if (isset($product['tops'])) {
+                foreach ($product['tops'] as $top) {
+                    if (!empty($top['band']) && !empty($top['cup'])) {
+                        $bandSizes[] = $top['band'];
+                        $cupSizes[] = $top['cup'];
+                        $possibleTopValues[] = [
+                            'band size' => $top['band'],
+                            'cup size' => $top['cup']
+                        ];
+                    } elseif (!empty($top['size'])) {
+                        $topSizes[] = $top['size'];
+                        $possibleTopValues[] = [
+                            'size' => $top['size']
+                        ];
+                    }
                 }
             }
 
-            $bandSizes = array_values(array_unique($bandSizes));
-            $cupSizes = array_values(array_unique($cupSizes));
+            if (isset($product['bottoms'])) {
+                foreach ($product['bottoms'] as $bottom) {
+                    if (!empty($bottom['size'])) {
+                        $bottomSizes[] = $bottom['size'];
+                        $possibleBottomValues[] = [
+                            'size' => $bottom['size']
+                        ];
+                    }
+                }
+            }
+        }
 
+        $bandSizes = array_values(array_unique($bandSizes));
+        $cupSizes = array_values(array_unique($cupSizes));
+        $topSizes = array_values(array_unique($topSizes));
+        $bottomSizes = array_values(array_unique($bottomSizes));
+
+        usort($bandSizes, function ($a, $b) use ($sizeWeights) {
+            if (!isset($sizeWeights[$a]) && !isset($sizeWeights[$b])) {
+                return 0;
+            } elseif (!isset($sizeWeights[$a])) {
+                return 1;
+            } elseif (!isset($sizeWeights[$b])) {
+                return -1;
+            } else {
+                return $sizeWeights[$a] - $sizeWeights[$b];
+            }
+        });
+
+        usort($cupSizes, function ($a, $b) use ($cupWeights) {
+            if (!isset($cupWeights[$a]) && !isset($cupWeights[$b])) {
+                return 0;
+            } elseif (!isset($cupWeights[$a])) {
+                return 1;
+            } elseif (!isset($cupWeights[$b])) {
+                return -1;
+            } else {
+                return $cupWeights[$a] - $cupWeights[$b];
+            }
+        });
+
+        usort($topSizes, function ($a, $b) use ($sizeWeights) {
+            if (!isset($sizeWeights[$a]) && !isset($sizeWeights[$b])) {
+                return 0;
+            } elseif (!isset($sizeWeights[$a])) {
+                return 1;
+            } elseif (!isset($sizeWeights[$b])) {
+                return -1;
+            } else {
+                return $sizeWeights[$a] - $sizeWeights[$b];
+            }
+        });
+
+        usort($bottomSizes, function ($a, $b) use ($sizeWeights) {
+            if (!isset($sizeWeights[$a]) && !isset($sizeWeights[$b])) {
+                return 0;
+            } elseif (!isset($sizeWeights[$a])) {
+                return 1;
+            } elseif (!isset($sizeWeights[$b])) {
+                return -1;
+            } else {
+                return $sizeWeights[$a] - $sizeWeights[$b];
+            }
+        });
+
+        if (!empty($bandSizes) && !empty($cupSizes)) {
             $transformedProduct[] = [
                 'type' => 'group',
                 'name' => 'top',
@@ -45,23 +133,21 @@ function productData()
                 ],
                 'possible_values' => $possibleTopValues
             ];
+        } elseif (!empty($topSizes)) {
+            $transformedProduct[] = [
+                'type' => 'group',
+                'name' => 'top',
+                'fields' => [
+                    [
+                        'label' => 'size',
+                        'values' => $topSizes
+                    ]
+                ],
+                'possible_values' => $possibleTopValues
+            ];
         }
 
-        if (isset($product['bottoms'])) {
-            $bottomSizes = [];
-            $possibleBottomValues = [];
-
-            foreach ($product['bottoms'] as $bottom) {
-                if (!empty($bottom['size'])) {
-                    $bottomSizes[] = $bottom['size'];
-                    $possibleBottomValues[] = [
-                        'size' => $bottom['size']
-                    ];
-                }
-            }
-
-            $bottomSizes = array_values(array_unique($bottomSizes));
-
+        if (!empty($bottomSizes)) {
             $transformedProduct[] = [
                 'type' => 'group',
                 'name' => 'bottom',
@@ -76,10 +162,9 @@ function productData()
         }
 
         if (!empty($transformedProduct)) {
-            $productsData[$productId] = $transformedProduct;
+            $productsData[$productName] = $transformedProduct;
         }
     }
-
 
     file_put_contents(__DIR__ . '/products.json', json_encode($productsData, JSON_PRETTY_PRINT));
 }
